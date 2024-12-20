a4c8bad99c2d1c5ab3c6a5d5754d2128f5bf95e7
Mike Bondar <mibon@fastmail.net>
2024-12-20 21:52:02 +0200

    Pull openocd config into the project

--- scripts/debug_server.sh
+++ scripts/debug_server.sh
@@ -1,1 +1,1 @@
-openocd -f /usr/share/openocd/scripts/interface/stlink.cfg -f /usr/share/openocd/scripts/target/stm32l4x.cfg
+openocd -f ./openocd/stlink.cfg -f ./openocd/stm32l4x.cfg
--- scripts/flash.sh
+++ scripts/flash.sh
@@ -1,1 +1,1 @@
-openocd -f /usr/share/openocd/scripts/interface/stlink.cfg -f /usr/share/openocd/scripts/target/stm32l4x.cfg -c "program ../build/basic.elf verify reset exit"
+openocd -f ./openocd/stlink.cfg -f ./openocd/stm32l4x.cfg -c "program ../build/basic.elf verify reset exit"
